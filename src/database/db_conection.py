08c134603d32b132050ad31d56ef3461612decb7
Mateus Souza <151884795+MattSouza14@users.noreply.github.com>
2024-12-03 10:20:46 -0300

    Update db_conection.py

--- src/database/db_conection.py
+++ src/database/db_conection.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import pandas as pd
 conn = mysql.connector.connect(
     host="localhost",  
     user="root",       
@@ -8,29 +9,40 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
-# cursor.execute("SELECT * FROM perfis")
+caminhodoarquivo = "C:/Users/ALUNO/Desktop/repositorio_sandro/detalhes_usuarios.xlsx"
 
-id = input("diga o id")
-idPerfil = input("diga o idPerfil")
-nome= input("diga o nome")
-numSeg = input("diga o numseg")
-numPost = input("diga o numposts")
-numSeguindo= input("diga o numseguindo")
+def lerInserirArquivo(caminhodoarquivo):
+    
+    df = pd.read_excel(caminhodoarquivo)
+    print(df.columns)
+    df.columns = df.columns.str.strip()
 
+    for index, row in df.iterrows():
+        try:
+            idPerfil = row['ID']
+            nome = row['Nickname']
+            numSeg = row['Seguidores']
+            numSeguindo = row['Seguindo']
+            numPost = row['Postagens']
+            if int(numPost) > 10 and int(numSeg) > 200:
+                cursor.execute("INSERT INTO users (ID, Nickname, Seguidores, Seguindo, Postagens) "
+                               "VALUES (%s, %s, %s, %s, %s)", 
+                               (idPerfil, nome, numSeg, numPost, numSeguindo))
+                conn.commit()
+                print(f"Dados inseridos para o id: {idPerfil}")
+            else:
+                print("Perfil não atende aos requisitos, que bad hein")
+        except KeyError as e:
+            print(f"Erro ao acessar a coluna: {e}") 
+try:
+   
+    lerInserirArquivo(caminhodoarquivo)
 
+    cursor.execute("SELECT * FROM users")
+    resultados = cursor.fetchall()
+    for row in resultados:
+        print(row)
 
-cursor.execute("INSERT INTO users (id, idPerfil, nome, numSeg, numPost, numSeguindo) "
-    "VALUES (%s, %s, %s, %s, %s, %s)", 
-    (id, idPerfil, nome, numSeg, numPost, numSeguindo))
-
-
-conn.commit()
-
-cursor.execute("SELECT * FROM users")
-resultados = cursor.fetchall()
-for row in resultados:
-    print(row)
-
-
-cursor.close()
-conn.close()
+finally:
+    cursor.close()
+    conn.close()
